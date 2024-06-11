cfcc099852f552d1e57819d9f5fa82faefa35819
FlyingMongoose <flyingmongoose@gmail.com>
2024-06-11 10:51:19 -0400

    feat: whitepaper page was added and embedded

---
+++ src/views/WPView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import WPComponent from '@/components/WPComponent.vue';
+</script>
+
+<template>
+  <div class="container is-centered">
+    <div class="columns">
+      <div class="column">
+        <WPComponent />
+      </div>
+    </div>
+  </div>
+</template>
