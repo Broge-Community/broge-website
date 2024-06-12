fdd4b8fd3a171a3978e08bb4364065349754d823
FlyingMongoose <flyingmongoose@gmail.com>
2024-06-12 00:19:56 -0400

    bugfix: attempting a compile fix

--- vite.config.ts
+++ vite.config.ts
@@ -26,11 +26,11 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
           if (id.includes('src/assets/scss')) {
             return 'styles';
+          }
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
           }
         }
       }
